a8c512e0eae9732901fa1a2eafc09df722644f1f
ShiroPoi <32829542+ShiroPoi@users.noreply.github.com>
2018-06-02 20:09:07 -0700

    Add files via upload

---
+++ src/Game/Type.java
@@ -0,0 +1,5 @@
+package Game;
+
+public enum Type {
+    BISHOP, ROOK, KING, PAWN, LANCE, KNIGHT, GOLD, SILVER
+}
---
+++ src/Pieces/Piece.java
@@ -0,0 +1,22 @@
+package Pieces;
+
+import Game.*;
+
+public abstract class Piece {
+
+    public int x,y;
+    public Player player;
+
+    public Piece(int x, int y, Player player) {
+        this.x = x;
+        this.y = y;
+        this.player = player;
+    }
+
+    public abstract Type getType();
+
+    public abstract boolean isValidPath(int finalX, int finalY);
+
+    public abstract int[][] drawPath(int startX, int startY, int finalX, int finalY);
+}
+
